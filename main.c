8ac6bec7ccb6747e233714610b68f75f53466182
Eyal Broder <itay.broder@gmail.com>
2020-07-24 15:26:11 +0300

    test

--- main.c
+++ main.c
@@ -31,28 +31,12 @@ void handle_assembly_files(char fileName[]){
     fclose(inputFile);
 	
 }
-char *substring(char *string, int position, int length)
+char *substring(char *string, int start, int length)
 {
-   char *pointer;
-   int c;
- 
-   pointer = malloc(length+1);
-   
-   if (pointer == NULL)
-   {
-      printf("Unable to allocate memory.\n");
-      exit(1);
-   }
- 
-   for (c = 0 ; c < length ; c++)
-   {
-      *(pointer+c) = *(string+position-1);      
-      string++;  
-   }
- 
-   *(pointer+c) = '\0';
- 
-   return pointer;
+    char* substr = malloc(length+1);
+    strncpy(substr, string+start, length);
+    substr[length] = '\0';
+    return substr;
 }
 
 /*This function adds to the dict*/
